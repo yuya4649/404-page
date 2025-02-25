1cf4ef2646f2a7d06ebc8f7f45bc9f88761d7c99
yuya4649 <saito091122@gmail.com>
2025-02-25 19:35:32 +0900

    Add config.js for SNS settings

---
+++ config.js
@@ -0,0 +1,9 @@
+// SNSアカウントの設定
+const CONFIG = {
+    // 実際に使用する際は、この値を変更してください
+    TWITTER_URL: 'https://x.com/front_yuya',
+    
+    // 将来的に他のSNSを追加する場合に備えて
+    // INSTAGRAM_URL: 'https://instagram.com',
+    // GITHUB_URL: 'https://github.com',
+}; 
